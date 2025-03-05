113c4cbd84f5d9c93eae9372ee7a75bc09945f7a
mohit-uniyal-dev <mohituniyal@frootystudios.com>
2025-03-05 16:52:16 +0530

    configured not found page and added to route

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,17 @@ import "./App.css";
 import MainLayout from "./pages/ResumeHome";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AdminPanel from "./pages/Admin Panel/AdminPanel";
+import NotFound from "./pages/NotFound";
 
 const App: React.FC = () => {
     return (
         <Router>
-          <Routes>
-            <Route path="/" element={<MainLayout />} />
-            <Route path="/admin" element={<AdminPanel />} />
-          </Routes>
+            <Routes>
+                <Route path="/" element={<MainLayout />} />
+                <Route path="/admin" element={<AdminPanel />} />
+                {/* Catch-all 404 Route (Must be at the end) */}
+                <Route path="*" element={<NotFound />} />
+            </Routes>
         </Router>
     );
 };
